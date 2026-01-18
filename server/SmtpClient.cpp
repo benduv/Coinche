ee2006c0f6b7f573af089177f3c675027d799062
Benjamin Duverlie <benjamin.duverlie@gmail.com>
2026-01-18 10:53:44 +0100

    fix build issue

---
+++ server/SmtpClient.cpp
@@ -0,0 +1,5 @@
+// SmtpClient.cpp
+// Ce fichier est nécessaire pour que Qt MOC génère correctement
+// les implémentations des signaux et métadonnées de la classe SmtpClient
+
+#include "SmtpClient.h"
