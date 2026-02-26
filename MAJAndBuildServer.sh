46f2d2b54230719ad7102fd0678f09b2bed3472d
Benjamin Duverlie <benjamin.duverlie@gmail.com>
2026-02-26 11:12:18 +0100

    update MAJAndBuildServer.sh for main

--- MAJAndBuildServer.sh
+++ MAJAndBuildServer.sh
@@ -1,7 +1,6 @@
 cd ~/Coinche
-git pull origin feature/android-support
+git pull origin main
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make server -j$(nproc)
 sudo systemctl restart coinche-server
-
