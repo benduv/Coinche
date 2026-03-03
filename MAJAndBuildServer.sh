8f6942c859cbe58891819c3760229de65ad0fc73
Benjamin Duverlie <benjamin.duverlie@gmail.com>
2026-03-03 11:06:42 +0100

    MAJAndBuildServer.sh

--- MAJAndBuildServer.sh
+++ MAJAndBuildServer.sh
@@ -1,6 +1,6 @@
-cd ~/Coinche
+cd ~/TestCoinche/Coinche
 git pull origin v0.2.0
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make server -j$(nproc)
-sudo systemctl restart coinche-server
+sudo systemctl restart test-coinche
